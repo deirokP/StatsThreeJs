a3aa47ad569246836cb6a79b4712e9a570cdcc6d
Jean <jean.pascault@accueilpro.com>
2021-06-12 17:38:18 +0200

    First Push

---
+++ src/Tableau.js
@@ -0,0 +1,27 @@
+class Tableau {
+    
+    constructor(nbElement, maxValue) {
+        this.minValue = Infinity;
+        this.maxValue = 0;
+        for (let i = 0; i < nbElement; i++) {
+            let randomValue = Math.random() * maxValue;
+            this.minValue = Math.min(randomValue, this.minValue);
+            this.maxValue = Math.max(randomValue, this.maxValue);
+            this.tableau.push(randomValue);
+        }
+    }
+
+    get getTableau() {
+        return this.tableau;
+    }
+
+    get getMin() {
+        return this.minValue;
+    }
+
+    get getMax() {
+        return this.maxValue;
+    }
+}
+
+export default Tableau
