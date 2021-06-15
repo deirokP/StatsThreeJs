5bff382cc30436672ac70faabe3b6c6d85775ef0
Jean <jean.pascault@accueilpro.com>
2021-06-15 16:25:17 +0200

    Ajout de fonction de calcul de couleur

--- src/Tableau.js
+++ src/Tableau.js
@@ -23,6 +23,41 @@ class Tableau {
     get getMax() {
         return this.maxValue;
     }
+
+    drawTable() {
+        for (let i = 0; i < this.tableau.length; i++) {
+            
+        }
+    }
+
+    // Returns a single rgb color interpolation between given rgb color
+    // based on the factor given; via https://codepen.io/njmcode/pen/axoyD?editors=0010
+    interpolateColor(color1, color2, factor) {
+        if (arguments.length < 3) { 
+            factor = 0.5; 
+        }
+        var result = color1.slice();
+        for (var i = 0; i < 3; i++) {
+            result[i] = Math.round(result[i] + factor * (color2[i] - color1[i]));
+        }
+        return result;
+    }
+
+    // My function to interpolate between two colors completely, returning an array
+    interpolateColors(color1, color2, steps) {
+        var stepFactor = 1 / (steps - 1),
+            interpolatedColorArray = [];
+    
+        color1 = color1.match(/\d+/g).map(Number);
+        color2 = color2.match(/\d+/g).map(Number);
+    
+        for(var i = 0; i < steps; i++) {
+            interpolatedColorArray.push(interpolateColor(color1, color2, stepFactor * i));
+        }
+    
+        return interpolatedColorArray;
+    }
+
 }
 
 export default Tableau
